9d5e4bb645dc49e5759bdae8f97df6a229be60b5
Francisco Oliveira <delley.fx@gmail.com>
2017-08-14 18:58:57 -0300

    fixes

---
+++ src/main/java/br/com/froli/greendogdelivery/controller/IndexController.java
@@ -0,0 +1,48 @@
+package br.com.froli.greendogdelivery.controller;
+
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/ambiente")
+	public String ambiente() {
+		return "ambiente";
+	}
+
+	@GetMapping("properties")
+	@ResponseBody
+	Properties properties() {
+		return System.getProperties();
+	}
+
+	@GetMapping("/delivery")
+	public String delivery() {
+		return "delivery/index";
+	}
+
+	/*@GetMapping("/profile")
+	@ResponseBody
+	public String[] profile() {
+		return this.environment.getActiveProfiles();
+	}*/
+
+	@GetMapping("/server")
+	@ResponseBody
+	public String server(HttpServletRequest request) {
+		return request.getServerName() + ":" + request.getServerPort();
+	}
+}
