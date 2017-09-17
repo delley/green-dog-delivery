8ccfb52298bbf868f831c5383d9cd43f12c171bc
Francisco Oliveira <delley.fx@gmail.com>
2017-09-17 20:26:37 -0300

    Fix data rest id

--- src/main/java/br/com/froli/greendogdelivery/config/SpringDataRestCustomization.java
+++ src/main/java/br/com/froli/greendogdelivery/config/SpringDataRestCustomization.java
@@ -1,17 +1,17 @@
 package br.com.froli.greendogdelivery.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.stereotype.Component;
 
+import br.com.froli.greendogdelivery.domain.Cliente;
 import br.com.froli.greendogdelivery.domain.Item;
-import br.com.froli.greendogdelivery.repository.ClienteRepository;
 
-@Component
+@Configuration
 public class SpringDataRestCustomization extends RepositoryRestConfigurerAdapter {
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.exposeIdsFor(Item.class, ClienteRepository.class);
+		config.exposeIdsFor(Item.class, Cliente.class);
 	}
 }
--- src/main/resources/static/js/delivery.js
+++ src/main/resources/static/js/delivery.js
@@ -1,50 +1,70 @@
 var app = angular.module("delivery", ["checklist-model"],
   function($locationProvider) {
     $locationProvider.html5Mode({
-      enabled : true,
+      enabled: true,
       requireBase: false
     })
-});
+  });
 
 app.controller('pedidoController',
   function($scope, $location, $http) {
+
     $scope.itens = [];
-    $scope.subTotal = 0;
-    $scope.pedidoItens = [];
-    var carregarItens = function () {
-      $http.get( "/api/itens")
-        .success(function (data) {
+    $scope.clientes = [];
+    $scope.pedido = {
+      cliente: {},
+      itens: [],
+      subTotal: 0
+    };
+
+    var carregarItens = function() {
+      $http.get("/api/itens")
+        .success(function(data) {
           $scope.itens = data["_embedded"]["itens"];
         })
-        .error(function (data, status) {
+        .error(function(data, status) {
           $scope.message = "Aconteceu um problema: " + data;
         });
     };
-    
-    $scope.fazerPedido = function(pedidoItens) {
-      $scope.message ="";
-      var pedidoStr="";
-      var prefixo="";
-      for (var i=0; i < $scope.pedidoItens.length; i++) {
-        pedidoStr += prefixo + $scope.pedidoItens[i].id;
+
+    var carregarClientes = function() {
+      $http.get("/api/clientes")
+        .success(function(data) {
+          $scope.clientes = data["_embedded"]["clientes"];
+        })
+        .error(function(data, status) {
+          $scope.message = "Aconteceu um problema: " + data;
+        });
+    }
+
+    $scope.fazerPedido = function(pedido) {
+      $scope.message = "";
+      var pedidoStr = "";
+      var prefixo = "";
+      for (var i = 0; i < pedido.itens.length; i++) {
+        pedidoStr += prefixo + pedido.itens[i].id;
         prefixo = ",";
       }
-      $scope.urlPedido="/rest/pedido/novo/2/"+pedidoStr;
-      $http.get( $scope.urlPedido)
-        .success(function (data) {
+      $scope.urlPedido = "/rest/pedido/novo/" + pedido.cliente.id + "/" + pedidoStr;
+      $http.get($scope.urlPedido)
+        .success(function(data) {
           $scope.idPedido = data["pedido"];
           $scope.mensagem = data["mensagem"];
           $scope.valorTotal = data["valorTotal"];
+          pedido.cliente = {};
+          pedido.itens = [];
+          pedido.subTotal = 0;
         })
-        .error(function (data, status) {
-    	  $scope.message = "Aconteceu um problema: " + "Status: " + data.status+ " - error: " + data.error;
-    	});
+        .error(function(data, status) {
+          $scope.message = "Aconteceu um problema: " + "Status: " + data.status + " - error: " + data.error;
+        });
     };
     $scope.isItemSelecionado = function() {
       if (this.checked)
-        $scope.subTotal+=this.i.preco;
+        $scope.pedido.subTotal += this.iten.preco;
       else
-        $scope.subTotal-=this.i.preco;
-      }
+        $scope.pedido.subTotal -= this.iten.preco;
+    }
+    carregarClientes();
     carregarItens();
-});
+  });
